addb958509f0233c20fc0fbde8d9bc3716861224
Andrew <andrew@andrew-desktop.(none)>
2009-12-21 23:35:52 -0800

    initial commit

---
+++ bin/apis/prog_web_scraper.rb
@@ -0,0 +1,39 @@
+require "rubygems"
+require "parsley"
+require "open-uri"
+require "pp"
+ 
+json_string = <<-STR
+  {
+    "apis (tr)": [ {
+      "name?": "td:nth-child(1)",
+      "url?": "td:nth-child(1) a @href",
+      "description?": "td:nth-child(2)",
+      "category?": "td:nth-child(3)"
+    } ]
+  }
+STR
+ 
+cache_file = File.join(File.dirname(__FILE__), "programmableweb.html")
+if File.exist?(cache_file)
+  content = File.read(cache_file)
+else
+  content = open("http://www.programmableweb.com/apis/directory").read
+  File.open(cache_file, 'w') do |file|
+    file.print content
+  end
+end
+
+parselet = Parsley.new(json_string)
+data = parselet.parse(:string => content)
+data['apis'].each do |api|
+  desc = api['description'] && api['description'].strip.squeeze(" ")
+  name = api['name'] && api['name'].strip.squeeze(" ")
+  text = "#{name} #{desc}".gsub(/((\w+)(\s+\2)+)/, '\2')
+  text = "#{name}".gsub(/((\w+)(\s+\2)+)/, '\2')
+  if text =~ /\bAPI\b/i
+    puts "API  #{text}"
+  else
+    puts "API  #{text} API"
+  end
+end
